30c3f29945b23aa5153e4d64ae2a943181da8032
Austin Byrd <byrda05@gmail.com>
2023-10-02 14:06:14 -0500

    update

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let num0 = 0;
+let random = Math.floor(Math.random() > num0)
+
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4;
+
+let set = [5, 10, 17, 20];
+let max = Math.max(...set);
